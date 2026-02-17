60f1b1e2854e4e963cbaa001c11f3ef259417b8c
meltjacTechs <ebwire02@gmail.com>
2026-02-17 23:25:14 +0300

    feat: add project preview image and community badge to project card in Home component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -70,8 +70,12 @@ export default function Home() {
         <div className="project-card group">
           <div className="preview">
             <img
-            src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" 
+            src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+            alt="Project preview" 
             />
+            <div className="badge">
+              <span>Community</span>
+            </div>
           </div>
         </div>
       </div>
