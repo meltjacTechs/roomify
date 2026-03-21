6077ea9bdeed97e77e2450110ee99fad10e1d338
meltjacTechs <ebwire02@gmail.com>
2026-03-21 12:34:43 +0300

    feat: update meta tags for Roomify title and description

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,8 +9,8 @@ import {createProject, getProjects} from "../../lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+        { title: "Roomify" },
+        { name: "description", content: "Roomify | AI-powered Architectural Visualization App" },
   ];
 }
 
